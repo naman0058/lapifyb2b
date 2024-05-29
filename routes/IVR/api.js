d2087fd00af01b500d12430dd9ace97c1fb2f92b
naman0058 <jnaman345@gmail.com>
2024-05-29 14:49:19 +0530

    fix-api

--- routes/IVR/api.js
+++ routes/IVR/api.js
@@ -66,6 +66,7 @@ const validateTableName = (req, res, next) => {
 };
 
 router.post('/insert', validateTableName, (req, res) => {
+    console.log('data comes')
     let { tablename, ...data } = req.body;
     pool.query(`INSERT INTO ?? SET ?`, [tablename, data], (err, result) => {
         if (err) {
