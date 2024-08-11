b1261bc22fde74dae611466c630fe82d3c4810d7
filemakrxpert <filemakrxpert@gmail.com>
2024-08-11 13:55:04 +0530

    a

--- routes/payment.js
+++ routes/payment.js
@@ -93,8 +93,106 @@ var instance = new Razorpay({
 
 
 
-   router.post('/razorpay-response', async (req, res) => {
-    let body = req.body;
+//    router.post('/razorpay-response', async (req, res) => {
+//     let body = req.body;
+  
+//     if (body.razorpay_payment_id && body.razorpay_order_id && body.razorpay_signature) {
+//       const data = req.query.orderid + '|' + body.razorpay_payment_id;
+//       let generated_signature = hmac_sha256(data, 'M3PlBQetVxVHN6SX3PkqtooV');
+  
+//       console.log('razorpayresponse',body)
+//       console.log('generated_signature',generated_signature)
+
+
+//       if (generated_signature == body.razorpay_signature) {
+//         body.orderid = req.query.orderid
+//         body.amount = req.query.amount;
+//         body.txnid = req.query.orderid;
+//         body.userid = req.query.userid;
+//         body.created_at = verify.getCurrentDate();
+  
+//         pool.query(`INSERT INTO payment_response SET ?`, body, async(err, result) => {
+//           if (err) throw err;
+//           else {
+//             try {
+//                 const orderid = req.query.orderid
+//                 const created_at = verify.getCurrentDate();
+//                 const userid = req.query.userid;
+//                 const address = req.query.address;
+        
+//                 // Fetch products from the cart
+//                 const cartResults = await queryAsync(`
+//                     SELECT c.*, 
+//                            (SELECT p.category FROM product p WHERE p.id = c.productid) AS category,
+//                            (SELECT p.price FROM product p WHERE p.id = c.productid) AS productprice
+//                     FROM cart c
+//                     WHERE c.userid = ? and quantity > 0`, [userid]);
+        
+//                 if (!Array.isArray(cartResults)) {
+//                     throw new Error('Failed to fetch cart results');
+//                 }
+        
+//                 let total_amount = 0;
+//                 let bookingData = [];
+        
+//                 // Calculate total amount and prepare booking data
+//                 cartResults.forEach(item => {
+//                     const amount = item.productprice * item.quantity;
+//                     total_amount += amount;
+        
+//                     bookingData.push({
+//                         userid,
+//                         orderid,
+//                         productid: item.productid,
+//                         amount,
+//                         created_at,
+//                         category: item.category,
+//                         quantity: item.quantity,
+//                         address,
+//                     status: 'pending',
+        
+//                     });
+//                 });
+        
+//                 // Insert booking data into 'booking' table
+//                 await Promise.all(bookingData.map(data => queryAsync(`INSERT INTO booking SET ?`, data)));
+        
+//                 // Insert order data into 'orders' table
+//                 const orderData = {
+//                     userid,
+//                     orderid,
+//                     created_at,
+//                     status: 'pending',
+//                     amount: total_amount,
+//                     address,
+//                     updated_at:created_at
+        
+//                 };
+//                 await queryAsync(`INSERT INTO orders SET ?`, orderData);
+//                 await queryAsync(`DELETE FROM cart WHERE userid = ? AND quantity > 0`, [userid]);
+        
+//                 res.json({ msg: 'success' });
+//             } catch (err) {
+//                 console.error(err);
+//                 res.status(500).json({ msg: 'An error occurred' });
+//             }
+
+
+//           }
+//         });
+//       } else {
+//         res.json({ msg: 'Unauthorized Payment' });
+//       }
+//     } else {
+//       res.json({ msg: 'Error Occurred' });
+//     }
+//   });
+
+
+
+
+router.get('/razorpay-success', async (req, res) => {
+    let body = req.query;
   
     if (body.razorpay_payment_id && body.razorpay_order_id && body.razorpay_signature) {
       const data = req.query.orderid + '|' + body.razorpay_payment_id;
